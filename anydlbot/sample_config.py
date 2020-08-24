d915d2d4a3c7c60247812ac306238fcea550542c
herrysis404 <67797468+herrysis404@users.noreply.github.com>
2020-08-24 21:13:21 +0800

    Update sample_config.py

--- anydlbot/sample_config.py
+++ anydlbot/sample_config.py
@@ -15,7 +15,7 @@ class Config(object):
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    FREE_USER_MAX_FILE_SIZE = 50000000000
     CHUNK_SIZE = 128
     PROCESS_MAX_TIMEOUT = 3600
     # default thumbnail to be used in the videos
